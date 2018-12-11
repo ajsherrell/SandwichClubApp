6baf9eb39272fce74ad2b491b4e3b5e2fae26f0e
AJ Sherrell <amiesherrell@gmail.com>
2018-12-10 20:36:58 -0500

    add JSON data to detailactivity

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,24 +3,40 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    // declare member variables\
+    private TextView originTextView;
+    private TextView descriptionTextView;
+    private TextView ingredientsTextView;
+    private TextView alsoKnowAsTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
+        //find image view
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+
+        // find text views
+        originTextView = findViewById(R.id.origin_tv);
+        descriptionTextView = findViewById(R.id.description_tv);
+        ingredientsTextView = findViewById(R.id.ingredients_tv);
+        alsoKnowAsTextView = findViewById(R.id.also_known_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +59,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +72,31 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        // set textview fields, if field is empty, hide the field
+        ingredientsTextView.setText(listArray(sandwich.getIngredients()));
+        descriptionTextView.setText(sandwich.getDescription());
 
+        if (sandwich.getAlsoKnownAs().isEmpty()) {
+            alsoKnowAsTextView.setVisibility(View.INVISIBLE);
+        } else {
+            alsoKnowAsTextView.setText(listArray(sandwich.getAlsoKnownAs()));
+        }
+
+        if (sandwich.getPlaceOfOrigin().isEmpty()) {
+            originTextView.setVisibility(View.INVISIBLE);
+        } else {
+            originTextView.setText(sandwich.getPlaceOfOrigin());
+        }
+    }
+
+    // make a string builder method
+    public StringBuilder listArray(List<String> list) {
+        StringBuilder stringBuilder = new StringBuilder();
+        // iterate through array
+        for (int i = 0; i < list.size(); i++) {
+            stringBuilder.append(list.get(i)).append("\n\n\n");
+        }
+        return stringBuilder;
     }
 }
